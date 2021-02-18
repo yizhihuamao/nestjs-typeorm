aa4876ec29e269b9e74a68823862ed4c5e61c979
fanda2013 <179030867@qq.com>
2021-02-18 12:11:44 +0800

    add port

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(MyLogger));
   app.use(helmet());
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
