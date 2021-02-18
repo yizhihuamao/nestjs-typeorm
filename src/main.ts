9609c4a1d2ee7bcfa3651e97a28520acd21ff34a
fanda2013 <179030867@qq.com>
2021-02-18 21:08:45 +0800

    remove scurf

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import * as helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
-import * as csurf from 'csurf';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(helmet(), cookieParser(), csurf({ cookie: true }));
+  app.use(helmet(), cookieParser());
   await app.listen(process.env.PORT);
 }
 bootstrap();
