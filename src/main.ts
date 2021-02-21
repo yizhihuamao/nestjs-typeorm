14ee80ce9f026462292745638a206a424845c847
fanda2013 <179030867@qq.com>
2021-02-21 08:19:44 +0800

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,21 @@ import * as helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet(), cookieParser());
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('猫 example')
+    .setDescription('The 猫 API description')
+    .setVersion('1.0')
+    .addTag('猫')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
